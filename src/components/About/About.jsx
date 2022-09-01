b90e038835ca92467c0bc81194812f84e540542a
ScorpioXKiller <dima.gorodov1999@gmail.com>
2022-09-01 14:23:41 +0300

    Edited info in the about section.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,15 +10,16 @@ const About = () => {
         <h2 className='about__title'>About me</h2>
         <p className='about__paragraph'>
           Hi! I am Dima Gorodov, a full-stack web developer. At the moment I’m
-          studying B.Sc. in Software Engineering and looking for a approprtiate
-          positions. I'm a very self-motivated person who always take any
-          opportunity to learn new things and always strive to achieve my goals.
+          studying B.Sc. in Software Engineering and also looking for
+          appropriate positions. I'm a very self-motivated person who always
+          take any opportunity to learn new things and I always strive to
+          achieve my goals.
         </p>
 
         <p className='about__paragraph'>
-          I'm focused on a MERN technology stack, but also have some expirence
-          in other programmer languages such as: C#, C++, Java. Also I know
-          about the design and architectural patterns.
+          I'm focused on a MERN technology stack, but also have some experience
+          in other programmer languages such as: C#, C++, Java. Also I have
+          knowledge in design patterns, architectural patterns and OOP.
         </p>
       </div>
     </section>
